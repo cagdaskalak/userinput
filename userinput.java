01d56f5b31701021b08a04ea8f9a7b0403660605
cagdaskalak <64699562+cagdaskalak@users.noreply.github.com>
2021-10-12 12:15:24 +0300

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        
+       
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favourite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+        
+
+    }
+}
